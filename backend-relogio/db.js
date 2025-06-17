397360263ff52d0b97b4d9703e8afecebecf7eb0
VeiledZ <enzob.gianini@gmail.com>
2025-06-17 20:47:37 -0300

    DB.JS modificado para nao gerar mais erros

--- backend-relogio/db.js
+++ backend-relogio/db.js
@@ -1,11 +1,14 @@
+// db.js
 const mongoose = require('mongoose');
 
 async function connectDB() {
-  await mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-  console.log('✅ Conectado ao MongoDB');
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log('✅ Conectado ao MongoDB');
+  } catch (err) {
+    console.error('❌ Erro ao conectar no MongoDB:', err);
+    process.exit(1);
+  }
 }
 
 module.exports = connectDB;
